5d19ff68ff6ea3d52746e25d3973a7386ce260e3
Duskatnity <alex.bernat99@gmail.com>
2024-06-26 09:28:41 +0200

    Last Ver

---
+++ api/src/migrations/20240617104422-create-companies-table.js
@@ -0,0 +1,48 @@
+'use strict'
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('companies', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      commercialAddress: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      fiscalAddress: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      commercialName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      fiscalName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      vatNumber: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        type: Sequelize.DATE
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('companies')
+  }
+}
