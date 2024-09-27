dee0134cac2da158faf0b6dc764d2f6bf3c816c3
Duskatnity <alex.bernat99@gmail.com>
2024-09-27 10:18:07 +0200

    Examen hecho

--- api/src/controllers/admin/routes-controller.js
+++ api/src/controllers/admin/routes-controller.js
@@ -2,7 +2,8 @@ exports.findAll = (req, res) => {
   const routes = {
     '/admin/usuarios': 'users.html',
     '/admin/empresas': 'companies.html',
-    '/admin/categorias-de-producto': 'productcategories.html'
+    '/admin/categorias-de-producto': 'productcategories.html',
+    '/admin/contactos': 'contacts.html'
   }
 
   res.status(200).send(routes)
--- api/src/models/contact.js
+++ api/src/models/contact.js
@@ -9,7 +9,7 @@ module.exports = function (sequelize, DataTypes) {
       },
       fingerprintId: {
         type: DataTypes.INTEGER,
-        allowNull: false,
+        allowNull: false
       },
       name: {
         type: DataTypes.STRING,
